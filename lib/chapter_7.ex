0ef642f80c9754d63cb443df2597ed9ef970aafe
yortz <skylarking420@gmail.com>
2013-07-18 18:46:42 +0200

    added list chapter

--- lib/chapter_7.ex
+++ lib/chapter_7.ex
@@ -2,4 +2,18 @@ defmodule Chapter7 do
 
   def sum([]), do: 0
   def sum([head|tail]), do: head + sum(tail)
+
+  def mapsum([], _func), do: 0
+  def mapsum([head|tail], func), do: func.(head) + mapsum(tail, func)
+
+  def listmax([x]), do: x
+  def listmax([head|tail]), do: max(head, listmax(tail))
+
+  @doc """
+    122 is z
+    iex> ?z
+  """
+  def caesar([], _n), do: []
+  def caesar([ head | tail ], n) when head+n <= 122, do: [ head+n | caesar(tail, n) ]
+  def caesar([ head | tail ], n), do: [ head+n-26 | caesar(tail, n)]
 end
--- test/chapter_7_test.exs
+++ test/chapter_7_test.exs
@@ -6,4 +6,17 @@ defmodule Chapter7Test do
   test "sum function without acc" do
     assert Chapter7.sum([1,2,3]) == 6
   end
+
+  test "mapsum" do
+    assert Chapter7.mapsum([1, 2, 3], &1 * &1) == 14
+  end
+
+  test "get maximum value in list" do
+    assert Chapter7.listmax([20, 3, 16]) == 20
+  end
+
+  test "adds n to each element of the list, if the addition results in a character greater than z." do
+    assert Chapter7.caesar('ryvkve', 13) == 'elixir'
+  end
+
 end
