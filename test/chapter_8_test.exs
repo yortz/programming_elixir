bd62eaa9f5dc8805b505135e6fb9bb35a72cdc7b
yortz <skylarking420@gmail.com>
2013-07-24 17:20:07 +0200

    anagram

--- test/chapter_8_test.exs
+++ test/chapter_8_test.exs
@@ -3,7 +3,14 @@ Code.require_file "test_helper.exs", __DIR__
 defmodule Chapter8Test do
   use ExUnit.Case, async: true
 
-  test "ASCII string" do
-    Chapter8.ascii('ab$£~04è')
+  test "return true if all chars in string are valid ASCII" do
+    #assert Chapter8.ascii('ab$£~04è') == true
+    assert Chapter8.ascii('ab$£~04è üÉ í') == false
+    assert Chapter8.ascii('') == false
+  end
+
+  test "anagram" do
+    assert Chapter8.anagram?('dog', 'god') == true
+    assert Chapter8.anagram?('dog', 'cat') == false
   end
 end
